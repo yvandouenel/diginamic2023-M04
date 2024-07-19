681714700f2c660524f4fcaedd11a146f858a9d7
yvandouenel <y.douenel@coopernet.fr>
2024-07-19 16:33:17 +0200

    Ajout de la gestion du son.

--- ionic/myApp/src/components/ExploreContainer.tsx
+++ ionic/myApp/src/components/ExploreContainer.tsx
@@ -1,17 +1,31 @@
 import './ExploreContainer.css';
 import { IonIcon } from '@ionic/react';
 import { alarmOutline } from 'ionicons/icons';
+import { useRef, useState } from 'react';
 
 interface ContainerProps {
   name: string;
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
-  const packmanSound = new Audio('/sound/pacman.wav');
+  const packmanSound = useRef(new Audio('/sound/pacman.wav'));
+  packmanSound.current.loop = true;
+  const [isPlaying, setIsplaying] = useState(false);
+
   const handleClickAlarm = () => {
     console.log(`Alarme cliquée`);
-    // On joue un son
-    packmanSound.play();
+    if (isPlaying) {
+      setIsplaying(false);
+      // On arrête le son
+      packmanSound.current.pause();
+    } else {
+      setIsplaying(true);
+      // On joue un son
+      packmanSound.current.play();
+    }
+
+
+
   }
   return (
     <div className="container">
@@ -22,7 +36,7 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
           onClick={() => { handleClickAlarm() }}
           icon={alarmOutline}
           size="large"
-          color="success"></IonIcon>
+          color={isPlaying ? "danger" : "success"}></IonIcon>
       </p>
     </div>
   );
