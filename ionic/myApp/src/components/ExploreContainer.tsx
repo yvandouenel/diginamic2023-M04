87abb77334f22290a0465d298011d0acb74cf82e
yvandouenel <y.douenel@coopernet.fr>
2024-07-19 14:49:10 +0200

    Sound pacman added.

--- ionic/myApp/src/components/ExploreContainer.tsx
+++ ionic/myApp/src/components/ExploreContainer.tsx
@@ -1,14 +1,29 @@
 import './ExploreContainer.css';
+import { IonIcon } from '@ionic/react';
+import { alarmOutline } from 'ionicons/icons';
 
 interface ContainerProps {
   name: string;
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  const packmanSound = new Audio('/sound/pacman.wav');
+  const handleClickAlarm = () => {
+    console.log(`Alarme cliquée`);
+    // On joue un son
+    packmanSound.play();
+  }
   return (
     <div className="container">
       <strong>{name} Nathan</strong>
       <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>
+        <IonIcon
+          onClick={() => { handleClickAlarm() }}
+          icon={alarmOutline}
+          size="large"
+          color="success"></IonIcon>
+      </p>
     </div>
   );
 };
